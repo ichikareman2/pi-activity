39030c77e6e4520d0a7d02d28c05e10ff17fef88
kevin quing <willardquing@gmail.com>
2024-01-30 22:43:34 +1100

    no email pir sense camera catch

---
+++ final-1-no-email/camera.py
@@ -0,0 +1,37 @@
+from picamera2 import Picamera2
+from libcamera import Transform
+import time
+import os
+
+class Camera():
+    FILE_FORMAT = "jpeg"
+    path = "/home/pi/Camera"
+    size = (1280, 720)
+    transform = Transform(vflip=True)
+    picam = None
+    file_name = "image_log"
+    last_file_name_number = None
+
+    def __init__(self, folder_path):
+        self.path = folder_path
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+            print("created folder: " + self.path)
+        print("starting up camera")
+        self.picam = Picamera2()
+        config = self.picam.create_still_configuration(main={"size": (self.size)}, transform=self.transform)
+        self.picam.configure(config)
+        self.picam.start()
+        time.sleep(2)
+        print("camera ready")
+
+    def capture(self):
+        new_file_name = self.path + "/" + str(time.time()) + "." + self.FILE_FORMAT
+        self.picam.capture_file(new_file_name)
+        print("captured image into: " + new_file_name)
+        return new_file_name
+
+    def dispose(self):
+        self.picam.stop()
+        print("picamera stopped and disposed")
+
